fe355c61b342511faaed687eab42d9a26bdb2f27
Nathan Ang <nathanang2000@gmail.com>
2015-05-24 22:54:31 -0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -15,18 +15,6 @@ if(isset($_FILES['file']))
 
 	$allowed = array('cpp', 'java', 'py');
 
-	if(in_array($file_ext, $allowed))
-	{
-		if($file_error == 0)
-		{
-			$file_name_new = uniqid('', true) . '.' . $file_ext . '/';
-			$file_dest = 'uploads/' . $file_name_new . '/';
-
-			if(move_uploaded_file($file_tmp, $file_destination))
-			{
-				echo "Successful Upload";
-			}
-		}
-	}
+	echo $allowed;
 
 }
