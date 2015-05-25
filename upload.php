04bde460bd65a15d60b6a010c756af208f399b7f
Nathan Ang <nathanang2000@gmail.com>
2015-05-24 22:44:07 -0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,5 +2,31 @@
 if(isset($_FILES['file']))
 {
 	$file = $_FILES['file'];
-	print_r($file);
+
+	//Properties
+	$file_name = $file['name'];
+	$file_tmp = $file['tmp_name'];
+	$file_size = $file['size'];
+	$file_error = $file['error'];
+
+	//file extension
+	$file_ext = explode('.', $file_name)
+	$file_ext = strtolower(end($file_ext));
+
+	$allowed = array('cpp', 'java', 'py');
+
+	if(in_array($file_ext, $allowed))
+	{
+		if($file_error == 0)
+		{
+			$file_name_new = uniqid('', true) . '.' . $file_ext;
+			$file_dest = 'uploads/' . $file_name_new;
+
+			if(move_uploaded_file($file_tmp, $file_destination))
+			{
+				echo "Successful Upload";
+			}
+		}
+	}
+
 }
