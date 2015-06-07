77e988b8301a5c546e41676b48368e517478833c
Nathan Ang <nathanang2000@gmail.com>
2015-06-07 11:03:52 -0700

    Update forms.php

--- forms.php
+++ forms.php
@@ -1,4 +1,17 @@
 <?php
-echo $_GET['name'];
 
+session_start();
+
+if(isset($_POST['name']))
+{
+	$msg = 'Name: ' .$POST['name'] ."\n"
+		.'Email: ' .$POST['inputEmail'] ."\n"
+		.'Message: ' ."\n" .$POST['textArea'] ."\n";
+	//mail('nathanang2000@gmail.com', 'New Contact Us Form', $msg);
+	echo $msg;
+}
+else
+{
+	exit(0);
+}
 ?>
